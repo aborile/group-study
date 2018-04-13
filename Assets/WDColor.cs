4b8e07504cb58f60cf6f7163ad6ea5898b084a11
DESKTOP-5A0ST27\SD <able312@korea.ac.kr>
2018-04-13 16:18:21 +0900

    Initial commit

---
+++ Assets/WDColor.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMYK
+{
+    public float c;
+    public float m;
+    public float y;
+    public float k;
+    /*
+    public CMYK()
+    {
+
+    }
+    public CMYK(float c, float m, float y, float k)
+    {
+        this.c = c;
+        this.m = m;
+        this.y = y;
+        this.k = k;
+    }
+    */
+    public CMYK add(CMYK cmyk)
+    {
+        CMYK val = new CMYK();
+        val.c = this.c + cmyk.c;
+        val.m = this.m + cmyk.m;
+        val.y = this.y + cmyk.y;
+        val.k = (this.k > cmyk.k ? cmyk.k : this.k);
+        if (val.c > 1) val.c = 1;
+        if (val.m > 1) val.m = 1;
+        if (val.y > 1) val.y = 1;
+        //if (val.k > 1) val.k = 1;
+        return val;
+    }
+    public void print()
+    {
+        Debug.Log("C: " + this.c);
+        Debug.Log("M: " + this.m);
+        Debug.Log("Y: " + this.y);
+        Debug.Log("K: " + this.k);
+    }
+}
+
+public class WDColor {
+    public bool self = true;
+    public CMYK rtoC(UnityEngine.Color rgb)
+    {
+        CMYK cmy = new CMYK();
+        float inR = rgb.r;
+        float inG = rgb.g;
+        float inB = rgb.b;
+        float r = inR / 255;
+        float g = inG / 255;
+        float b = inB / 255;
+        float k;
+        if (r > g)
+        {
+            if (r > b) { k = 1 - r; }
+            else { k = 1 - b; }
+        }
+        else if (g > b) { k = 1 - g; }
+        else { k = 1 - b; }
+        cmy.c = (1 - r - k) / (1 - k);
+        cmy.m = (1 - g - k) / (1 - k);
+        cmy.y = (1 - b - k) / (1 - k);
+        cmy.k = k;
+        if (cmy.c > 1) cmy.c = 1;
+        if (cmy.c < 0) cmy.c = 0;
+        if (cmy.m > 1) cmy.m = 1;
+        if (cmy.m < 0) cmy.m = 0;
+        if (cmy.y > 1) cmy.y = 1;
+        if (cmy.y < 0) cmy.y = 0;
+        if (cmy.k > 1) cmy.k = 1;
+        if (cmy.k < 0) cmy.k = 0;
+        return cmy;
+    }
+
+    public UnityEngine.Color ctoR(CMYK cmyk)
+    {
+        float c = cmyk.c;
+        float m = cmyk.m;
+        float y = cmyk.y;
+        float k = cmyk.k;
+        UnityEngine.Color rgb = new UnityEngine.Color();
+        rgb.r = 255 * (1 - c) * (1 - k);
+        rgb.g = 255 * (1 - m) * (1 - k);
+        rgb.b = 255 * (1 - y) * (1 - k);
+        if (rgb.r > 255) rgb.r = 255;
+        else if (rgb.r < 0) rgb.r = 0;
+        if (rgb.g > 255) rgb.g = 255;
+        else if (rgb.g < 0) rgb.g = 0;
+        if (rgb.b > 255) rgb.b = 255;
+        else if (rgb.b < 0) rgb.b = 0;
+        return rgb;
+    }
+}
---
+++ Assets/bound.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bound : MonoBehaviour
+{
+    
+    //public float thrust = 1;
+    public Rigidbody player;
+    public Renderer pRen, cRen;
+    public WDColor chg;
+
+    /*
+    public static float gravy = Physics.gravity.y;
+    public static float len = (float)1.9 * 6;
+    public static float v = Mathf.Sqrt(2 * len * gravy / Mathf.Sqrt(3));
+    public float valy = v * Mathf.Sqrt(3) / 2;
+    public float valz = v / 2;
+    */
+    public float gos, gou;
+    // Use this for initialization
+    void Start()
+    {
+        gos = (float)5.2886338279088022816502776210371;
+        gou = (float)9.1601824925655238128142625022436;
+        chg = new WDColor();
+        player = GetComponent<Rigidbody>();
+        //player.velocity = new Vector3(0, 0, valz);
+        player.velocity = new Vector3(0, 0, gos);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.AddForce(transform.forward * thrust);
+        //rb.AddForce(0, 0, thrust, ForceMode.Impulse);
+        //transform.position += Vector3.forward;
+        //if (Input.GetButtonDown("Jump"))
+        /*
+        if (player.position.y <= 1)
+        {
+            player.velocity = new Vector3(0, 10, 5);
+        }
+        */
+        if (Input.GetKey(KeyCode.A)){
+            /*
+            Vector3 tmpv = player.velocity;
+            tmpv[0] -= (float)0.2;
+            player.velocity = tmpv;
+            */
+            //Debug.Log(player.velocity);
+            player.AddForce(Vector3.left * 3f);
+            //거리길어질수잇음
+            //Debug.Log(player.velocity);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            /*
+            Vector3 tmpv = player.velocity;
+            tmpv[0] += (float)0.2;
+            player.velocity = tmpv;
+            */
+            //Debug.Log(player.velocity);
+            player.AddForce(Vector3.right * 3f);
+            //Debug.Log(player.velocity);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Land")
+        {
+            //Debug.Log(player.velocity);
+            //player.velocity = new Vector3(player.velocity.x, valy, valz);
+            player.velocity = new Vector3(player.velocity.x, gou, player.velocity.z);
+            //player.AddForce(0, (-1)*player.velocity.y, 0);
+            //Debug.Log(player.velocity);
+
+            pRen = player.gameObject.GetComponent<Renderer>();
+            Color pcRGB = pRen.material.GetColor("_Color");
+            CMYK pcCMY = chg.rtoC(pcRGB);
+
+            cRen = col.gameObject.GetComponent<Renderer>();
+            Color ccRGB = cRen.material.GetColor("_Color");
+            CMYK ccCMY = chg.rtoC(ccRGB);
+
+            Color newc = chg.ctoR(pcCMY.add(ccCMY));
+            pRen.material.SetColor("_Color", newc);
+            //그리고 타일높이는 6이 맞을까 거지같은 포물선...
+        }
+    }
+}
