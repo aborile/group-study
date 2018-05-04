716a6f9fbb16528eb99677790fa74f00ad822414
DESKTOP-5A0ST27\SD <able312@korea.ac.kr>
2018-05-04 22:46:18 +0900

    parser 구현
    string[] 형식의 List 안에 타일 7개씩에 대한 정보를 담음.

--- Assets/parser.cs
+++ Assets/parser.cs
@@ -4,15 +4,39 @@ using System.IO;
 using UnityEngine;
 
 public class parser : MonoBehaviour {
-
+    public List<string []> arr; 
 	// Use this for initialization
 	void Start () {
+        /* 0: 빈 타일, 1: 일반 타일, 2: 특수 타일 */
         StreamReader input = new StreamReader("map.txt");
-        Debug.Log(input.ReadLine());
+        //Debug.Log(input.ReadLine());
+        Reader(input);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void Reader (StreamReader t)
+    {
+        string text;
+        string[] sp;
+        arr = new List<string []>();
+        while (t.Peek() >= 0)
+        {
+            text = t.ReadLine();
+            sp = text.Split(' ');      //공백 단위로 배열에 저장
+            arr.Add(sp);               //List arr 안에 배열 저장
+        }
+        /*
+        for (int i = 0; i < arr.Count; i++)
+        {
+            for (int j = 0; j < arr[i].Length; j++)
+            {
+                Debug.Log(arr[i][j]);
+            }
+        }
+        */
+    }
 }
